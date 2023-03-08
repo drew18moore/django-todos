11d5aa60421ce3b3901d9b2cc9aacf958aa2e48e
Drew <dcm752052@gmail.com>
2023-03-08 15:34:52 -0500

    Changed todoForm to use form from forms.py. Form now works

--- todos/models.py
+++ todos/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Todo(models.Model):
-    author = models.CharField(max_length=50)
+    # author = models.CharField(max_length=50)
     body = models.CharField(max_length=256)
     created_at = models.DateTimeField(auto_now_add=True)
--- todos/views.py
+++ todos/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
+from .forms import TodoForm
 
 # Create your views here.
 def index(request):
-    return render(request, "todoForm.html")
+    if request.method == "POST":
+        form = TodoForm(request.POST)
+
+        if form.is_valid():
+          body = request.POST["body"]
+          newTodo = Todo.objects.create(body=body)
+          newTodo.save()
+    else:
+        form = TodoForm()
+    return render(request, "todoForm.html", { "form": form })
+
+def newTodo(request):
+    print("NEW TODO")
+    body = request.POST["body"]
+    newTodo = Todo.objects.create(body=body)
+    newTodo.save()
+    return HttpResponse("New todo created")
