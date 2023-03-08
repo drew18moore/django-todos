159061d31b6850c53f820fce09e19a47345692c8
Drew <dcm752052@gmail.com>
2023-03-08 14:32:21 -0500

    Created model for todo. Registered it to admin site.

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    author = models.CharField(max_length=50)
+    body = models.CharField(max_length=256)
+    created_at = models.DateTimeField(auto_now_add=True)
