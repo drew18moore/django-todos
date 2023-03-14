a3ee89ecba3115bf1ab1882fb46e6489a77c7c06
Drew <dcm752052@gmail.com>
2023-03-13 23:49:16 -0400

    Set up user login. Displays error message when user types incorrect username or password.

--- todos/urls.py
+++ todos/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("login", views.login, name="login"),
+    path("login", views.login_view, name="login"),
     path("register", views.register, name="register"),
     path("todo/<int:todo_id>/delete/", views.delete_todo, name="delete_todo"),
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from .models import *
 from .forms import TodoForm, LoginForm, RegisterForm
@@ -9,27 +10,36 @@ def index(request):
         form = TodoForm(request.POST)
 
         if form.is_valid():
-          body = request.POST["body"]
-          newTodo = Todo.objects.create(body=body)
-          newTodo.save()
-          return redirect("index")  # Prevents form from resubmitting on refresh
+            body = request.POST["body"]
+            newTodo = Todo.objects.create(body=body)
+            newTodo.save()
+            return redirect("index")  # Prevents form from resubmitting on refresh
     form = TodoForm()
     allTodos = Todo.objects.all()
     return render(request, "todoForm.html", { "form": form, "todos": allTodos[::-1] })
 
 def register(request):
-   if request.method == "POST":
-      return redirect("register")
-   form = RegisterForm()
-   return render(request, "register.html", { "form": form })
+    if request.method == "POST":
+        return redirect("register")
+    form = RegisterForm()
+    return render(request, "register.html", { "form": form })
 
-def login(request):
-   if request.method == "POST":
-      return redirect("login")
-   form = LoginForm()
-   return render(request, "login.html", { "form": form })
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        print(username, password)
+        user = authenticate(request, username=username, password=password)
+        print(user)
+        if user is None:
+            form = LoginForm()
+            return render(request, "login.html", { "form": form, "error": "Invalid username or password" })
+        login(request, user)
+        return redirect("/")
+    form = LoginForm()
+    return render(request, "login.html", { "form": form })
 
 def delete_todo(request, todo_id):
-   todo = Todo.objects.get(id=todo_id)
-   todo.delete()
-   return redirect("index")
+    todo = Todo.objects.get(id=todo_id)
+    todo.delete()
+    return redirect("index")
